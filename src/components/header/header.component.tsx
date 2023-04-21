ed8ba2a25e6608cd2df0f63b8522ed4c2c8fd63d
aliceb91 <alice.birkett@outlook.com>
2023-04-21 12:31:28 +0100

    un-birds homepage

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.scss';
 export function Header() {
   return (
     <header className={styles.header}>
-      <span className={styles.span}>BirdsHomePage</span>
+      <span className={styles.span}>TKNotes</span>
     </header>
   )
 }
